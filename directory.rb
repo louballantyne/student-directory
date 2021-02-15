70e6e924272495f177ee37357bbd3e4532037f5a
Louise Ballantyne <lou@birdline.co.uk>
2021-02-15 15:36:57 +0000

    updated README to add student count

---
+++ directory.rb
@@ -0,0 +1,19 @@
+# print student list
+puts "The students of Villains Academy"
+puts "-------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+# print number of students
+puts "Overall, we have "
+print 11
+puts " great students"
+print "line1\nline2\nline3"
