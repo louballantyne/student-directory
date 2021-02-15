0bfbfd7ed5ad513772f0d5fc27ba6bf9b6575431
Louise Ballantyne <lou@birdline.co.uk>
2021-02-15 19:40:57 +0000

    updated directory center text and to prevent the program asking for more names when the user wanted to quit

--- directory_more_fields.rb
+++ directory_more_fields.rb
@@ -1,37 +1,36 @@
 # print header inc. name of Academy
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
 end
 
 #print list of students
 def print(students)
   students.each_with_index do |student_info, index|
-    puts "#{index+1}. #{student_info[:name]} (#{student_info[:cohort]} cohort)."
-    puts "#{student_info[:name]} is #{student_info[:height]} tall, was born in #{student_info[:country]} and #{student_info[:pronoun]} likes #{student_info[:hobbies]}."
+    puts "#{index+1}. #{student_info[:name]} (#{student_info[:cohort]} cohort).".center(50)
+    puts "#{student_info[:name]} is #{student_info[:height]} tall, was born in #{student_info[:country]} and #{student_info[:pronoun]} likes #{student_info[:hobbies]}.".center(50)
   end
 end
 # print number of students
 def print_footer(student_info)
-  puts "Overall, we have #{student_info.length} great students"
+  puts "Overall, we have #{student_info.length} great students".center(50)
 end
 
 # method to obtain cohort list from user
 
 def input_students
   students = Array.new
-  puts "To finish, just hit return twice"
-  while true
-    puts "What is the name of the student?"
+  puts "What is the name of the student?".center(50)
+    while true
     inputname = gets.chomp
     break if inputname == ""
-    puts "What is #{inputname}'s country of birth?"
+    puts "What is #{inputname}'s country of birth?".center(50)
     inputcountry = gets.chomp
-    puts "What pronoun do they prefer?"
+    puts "What pronoun do they prefer?".center(50)
     inputpronoun = gets.chomp
-    puts "What is #{inputname}'s height?"
+    puts "What is #{inputname}'s height?".center(50)
     inputheight = gets.chomp
-    puts "What are #{inputname}'s hobbies?"
+    puts "What are #{inputname}'s hobbies?".center(50)
     inputhobbies = gets.chomp
 
     students << {
@@ -42,7 +41,8 @@ def input_students
       height: inputheight,
       hobbies: inputhobbies
     }
-    puts "Now, we have #{students.length} students"
+    puts "Now, we have #{students.length} students. To add more, type another name.".center(50)
+    puts "Otherwise, press enter.".center(50)
   end
   return students
 end
