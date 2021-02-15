01f50449c829ed755dcee1de0b93083df058f6a3
Louise Ballantyne <lou@birdline.co.uk>
2021-02-15 16:00:33 +0000

    updated directory to use hashes

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 # declare variables
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 # print header inc. name of Academy
 def print_header
@@ -19,12 +19,12 @@ def print_header
 end
 
 #print list of students
-def print(names)
-  names.each { |student| puts student }
+def print(students)
+  students.each { |student_info| puts "#{student_info[:name]}, (#{student_info[:cohort]} cohort)" }
 end
 # print number of students
-def print_footer(names)
-  puts "Overall, we have #{names.length} great students"
+def print_footer(student_info)
+  puts "Overall, we have #{student_info.length} great students"
 end
 
 print_header
