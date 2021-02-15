10c7313a30066fb30b3fa928ddcf9bc52aafc598
Louise Ballantyne <lou@birdline.co.uk>
2021-02-15 16:18:04 +0000

    updated directory to take input from user

--- directory.rb
+++ directory.rb
@@ -1,17 +1,3 @@
-# declare variables
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
 # print header inc. name of Academy
 def print_header
   puts "The students of Villains Academy"
@@ -27,6 +13,22 @@ def print_footer(student_info)
   puts "Overall, we have #{student_info.length} great students"
 end
 
+# method to obtain cohort list from user
+
+def input_students
+  students = Array.new
+  puts "What is the name of the student?"
+  puts "To finish, just hit return twice"
+  while true
+    input = gets.chomp
+    break if input == ""
+    students << {name: input, cohort: :november}
+    puts "Now, we have #{students.length} students"
+  end
+  return students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
