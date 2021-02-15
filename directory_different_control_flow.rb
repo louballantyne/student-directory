8300258aad2ec973f1aea22425c3f2bb812d3112
Louise Ballantyne <lou@birdline.co.uk>
2021-02-15 19:18:07 +0000

    updated directory to use until control flow to print students

---
+++ directory_different_control_flow.rb
@@ -0,0 +1,38 @@
+# print header inc. name of Academy
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+#print list of students
+def print(students)
+  i = 0
+  until i == students.length
+    puts "#{i+1}. #{students[i][:name]}, (#{students[i][:cohort]} cohort)"
+    i += 1
+  end
+end
+# print number of students
+def print_footer(student_info)
+  puts "Overall, we have #{student_info.length} great students"
+end
+
+# method to obtain cohort list from user
+
+def input_students
+  students = Array.new
+  puts "What is the name of the student?"
+  puts "To finish, just hit return twice"
+  while true
+    input = gets.chomp
+    break if input == ""
+    students << {name: input, cohort: :november}
+    puts "Now, we have #{students.length} students"
+  end
+  return students
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
